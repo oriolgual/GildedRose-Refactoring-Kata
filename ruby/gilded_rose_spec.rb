9e722cdd52fb6cf2a78ed0a2fea3e237816c48cd
Oriol Gual <oriol.gual@gmail.com>
2013-09-29 12:39:27 +0200

    Cosmetic changes to specs

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -4,18 +4,18 @@ describe GildedRose do
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       items[0].name.should == "foo"
     end
 
     it 'decreases the sell in by one' do
       items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       items[0].sell_in.should eq(-1)
     end
 
     context 'when its a regular item' do
-      context "when the sell in date hasn't passed" do
+      context "and the sell in date hasn't passed" do
         it 'decreases the quality' do
           items = [Item.new("foo", 1, 10)]
           GildedRose.new(items).update_quality
@@ -23,7 +23,7 @@ describe GildedRose do
         end
       end
 
-      context "when the sell in date has passed" do
+      context "and the sell in date has passed" do
         it 'decreases the quality twice faster' do
           items = [Item.new("foo", 0, 10)]
           GildedRose.new(items).update_quality
@@ -31,7 +31,7 @@ describe GildedRose do
         end
       end
 
-      context 'when the item has no value' do
+      context 'and the item has no value' do
         it "doesn't decrease the quality" do
           items = [Item.new("foo", 0, 0)]
           GildedRose.new(items).update_quality
@@ -79,14 +79,14 @@ describe GildedRose do
     end
 
     context "when the item is a backstage ticket" do
-      context "when the sell in date hasn't passed" do
+      context "and the sell in date hasn't passed" do
         it "doesn't increase its value over 50" do
           items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 50)]
           GildedRose.new(items).update_quality
           items[0].quality.should == 50
         end
 
-        context "when there are more then 10 for the concert" do
+        context "and there are more then 10 days for the concert" do
           it 'increases its value by 1' do
             items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 5)]
             GildedRose.new(items).update_quality
@@ -94,7 +94,7 @@ describe GildedRose do
           end
         end
 
-        context "when there are 10 days or less for the concert" do
+        context "and there are 10 days or less for the concert" do
           it 'increases its value by 2' do
             items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 5)]
             GildedRose.new(items).update_quality
@@ -102,7 +102,7 @@ describe GildedRose do
           end
         end
 
-        context "when there are 5 days or less for the concert" do
+        context "and there are 5 days or less for the concert" do
           it 'increases its value by 3' do
             items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 5)]
             GildedRose.new(items).update_quality
@@ -119,19 +119,19 @@ describe GildedRose do
         end
       end
 
-      context 'when the item is a conjured item' do
+      context 'and the item is a conjured item' do
         it 'decreases the quality by two each day' do
           items = [Item.new("Conjured Mana Cake", 1, 10)]
           GildedRose.new(items).update_quality
           items[0].quality.should == 8
         end
-      end
 
-      context "when the sell in date has passed" do
-        it 'decreases the quality by four each day' do
-          items = [Item.new("Conjured Mana Cake", 0, 10)]
-          GildedRose.new(items).update_quality
-          items[0].quality.should == 6
+        context "and the sell in date has passed" do
+          it 'decreases the quality by four each day' do
+            items = [Item.new("Conjured Mana Cake", 0, 10)]
+            GildedRose.new(items).update_quality
+            items[0].quality.should == 6
+          end
         end
       end
     end
