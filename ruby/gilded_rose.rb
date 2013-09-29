c85ead53af79157423138fa0f71cb59c49fb6b37
Oriol Gual <oriol.gual@gmail.com>
2013-09-29 12:09:52 +0200

    First pass refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,50 +6,74 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
+      next if immutable_item?(item)
+
+      if regular_item?(item)
+        decrease_quality(item)
+      elsif can_increase_quality?(item)
+        increase_quality(item)
+
+        if backstage_ticket?(item)
+          increase_quality(item) if item.sell_in < 11
+          increase_quality(item) if item.sell_in < 6
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+
+      make_day_pass(item)
+
+      if sell_date_passed?(item)
+        if backstage_ticket?(item)
+          reset_quality(item)
+        elsif aged_brie?(item)
+          increase_quality(item)
+        elsif regular_item?(item)
+          decrease_quality(item)
         end
       end
     end
+  end
+
+  private
+  def make_day_pass(item)
+    item.sell_in -= 1
+  end
+
+  def immutable_item?(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def backstage_ticket?(item)
+    item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def aged_brie?(item)
+    item.name == "Aged Brie"
+  end
+
+  def regular_item?(item)
+    !(aged_brie?(item) || backstage_ticket?(item))
+  end
+
+  def decrease_quality(item)
+    return if item.quality == 0
+    item.quality -= 1
+  end
+
+  def increase_quality(item)
+    return if item.quality == 50
+    item.quality += 1
+  end
+
+  def reset_quality(item)
+    item.quality = 0
+  end
+
+  def sell_date_passed?(item)
+    item.sell_in < 0
+  end
+
+  def can_increase_quality?(item)
+    !regular_item?(item) && item.quality < 50
   end
 end
 
